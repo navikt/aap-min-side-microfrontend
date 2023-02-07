9d352950c1ea548b783f22f9bded38773fa064da
vetleh <vetle.hollund@nav.no>
2023-02-07 13:16:22 +0100

    :lipstick: flexbox chevron

--- src/components/Komponent.tsx
+++ src/components/Komponent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Detail, Heading, Panel, Tag } from "@navikt/ds-react";
+import { Next } from "@navikt/ds-icons";
 import "./Komponent.css";
 import { format } from "date-fns";
 
@@ -10,21 +11,24 @@ interface Props {
 
 const Komponent = ({ mottatt, manglerVedlegg }: Props) => {
   return (
-    <div className="">
-      <Panel border>
-        <Heading level="3" size="small" className="heading">
-          Søknad om AAP (arbeidsavklaringspenger)
-        </Heading>
-        <Detail className="detail">Mottatt: {format(mottatt, "dd.MM.yyyy hh:mm")}</Detail>
-        {manglerVedlegg ? (
-          <Tag variant="warning" size="small">
-            Dokumentasjon mangler
-          </Tag>
-        ) : (
-          <Tag variant="neutral" size="small">
-            Vi tar kontakt om vi trenger mer informasjon
-          </Tag>
-        )}
+    <div>
+      <Panel className="flexContainer" border>
+        <div>
+          <Heading level="3" size="small" className="heading">
+            Søknad om AAP (arbeidsavklaringspenger)
+          </Heading>
+          <Detail className="detail">Mottatt: {format(mottatt, "dd.MM.yyyy hh:mm")}</Detail>
+          {manglerVedlegg ? (
+            <Tag variant="warning" size="small">
+              Dokumentasjon mangler
+            </Tag>
+          ) : (
+            <Tag variant="neutral" size="small">
+              Vi tar kontakt om vi trenger mer informasjon
+            </Tag>
+          )}
+        </div>
+        <Next />
       </Panel>
     </div>
   );
