fc30e08db5f71ecaf0fb22ed49926ce789ec0f78
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2023-02-07 10:41:06 +0100

    CORS headere og korrigering av basepath for server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const basePath = "/aap-min-side-microfrontend";
+const basePath = "/aap/aap-min-side-microfrontend";
 const buildPath = path.resolve(__dirname, "../dist");
 const server = express();
 const corsAllowedOrigin = process.env.CORS_ALLOWED_ORIGIN;
