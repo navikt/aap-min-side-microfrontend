bd14241c1f1c5d90ea6db9c2921ba3a9b0175d91
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2023-02-14 11:07:37 +0100

    :lipstick: Oppdaterer mikrofronten til å bruke samme type styling som andre mikrofrontends på min side

--- src/components/Komponent.tsx
+++ src/components/Komponent.tsx
@@ -3,6 +3,7 @@ import { BodyShort, Heading, LinkPanel, Tag } from "@navikt/ds-react";
 import { format } from "date-fns";
 import { mineAapUrl } from "../api/urls";
 import styles from "./Komponent.module.css";
+import LinkCard from "./card/LinkCard";
 
 interface Props {
   mottatt: Date;
@@ -14,18 +15,23 @@ const Komponent = ({ mottatt, manglerVedlegg, søknadId }: Props) => {
   const url = mineAapUrl; //`${mineAapUrl}${manglerVedlegg ? `${søknadId}/ettersendelse/` : ""}`;
 
   return (
-    <div>
-      <LinkPanel className={styles.panel} href={url} border={false}>
-        <Heading level="3" size="small" className={styles.heading}>
-          Søknad om AAP (arbeidsavklaringspenger)
-        </Heading>
-        <BodyShort className={styles.detail}>Mottatt: {format(mottatt, "dd.MM.yyyy hh:mm")}</BodyShort>
-        {manglerVedlegg ? (
-          <Tag variant="warning">Dokumentasjon mangler</Tag>
-        ) : (
-          <Tag variant="neutral">Vi tar kontakt om vi trenger mer informasjon</Tag>
-        )}
-      </LinkPanel>
+    <div className={styles.mikrofrontend}>
+      <Heading className={styles.mikrofrontendHeader} level="2" size="medium">
+        Arbeidsavklarings&shy;penger
+      </Heading>
+      <LinkCard href={url}>
+        <>
+          <Heading level="3" size="small" className={styles.heading}>
+            Søknad om AAP (arbeidsavklarings&shy;penger)
+          </Heading>
+          <BodyShort className={styles.detail}>Mottatt: {format(mottatt, "dd.MM.yyyy hh:mm")}</BodyShort>
+          {manglerVedlegg ? (
+            <Tag variant="warning">Dokumentasjon mangler</Tag>
+          ) : (
+            <Tag variant="neutral">Vi tar kontakt om vi trenger mer informasjon</Tag>
+          )}
+        </>
+      </LinkCard>
     </div>
   );
 };
