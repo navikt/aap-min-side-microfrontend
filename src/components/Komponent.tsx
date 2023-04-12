d85148f81d8a9533e5d78e4d9538bfccd287ea8c
VHollund <vetlehoo@gmail.com>
2023-04-12 11:17:07 +0200

    :globe_with_meridians:språk støtte

--- src/components/Komponent.tsx
+++ src/components/Komponent.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BodyShort, Heading, Tag } from "@navikt/ds-react";
 import { format } from "date-fns";
 import { mineAapUrl } from "../api/urls";
 import styles from "./Komponent.module.css";
 import LinkCard from "./card/LinkCard";
+import { LanguageContext } from "../provider/LanguageProvider";
+import { text } from "../translations/text";
 
 interface Props {
   mottatt: Date;
@@ -12,24 +14,24 @@ interface Props {
 
 const Komponent = ({ mottatt, manglerVedlegg }: Props) => {
   const url = mineAapUrl; //`${mineAapUrl}${manglerVedlegg ? `${søknadId}/ettersendelse/` : ""}`;
-
+  const language = useContext(LanguageContext);
   return (
     <div className={styles.mikrofrontend}>
       <LinkCard href={url}>
         <>
           <Heading level="3" size="small" className={styles.heading}>
-            Søknad om AAP (arbeidsavklarings&shy;penger)
+            {text.card.heading[language]}
           </Heading>
           <BodyShort size="small" className={styles.detail}>
-            Mottatt: {format(mottatt, "dd.MM.yyyy hh:mm")}
+            {text.card.recieved[language]} {format(mottatt, "dd.MM.yyyy hh:mm")}
           </BodyShort>
           {manglerVedlegg ? (
             <Tag variant="warning" size="small">
-              Dokumentasjon mangler
+              {text.card.dokumentasjonMangler[language]}
             </Tag>
           ) : (
             <Tag variant="neutral" size="small">
-              Vi tar kontakt om vi trenger mer informasjon
+              {text.card.tarKontakt[language]}
             </Tag>
           )}
         </>
---
+++ src/translations/text.ts
@@ -0,0 +1,24 @@
+export const text = {
+  heading: {
+    nn: "Arbeidsavklaringspengar",
+    nb: "Arbeidsavklaringspenger",
+  },
+  card: {
+    heading: {
+      nn: "Søknad om AAP (arbeidsavklaringspengar)",
+      nb: "Søknad om AAP (arbeidsavklaringspenger)",
+    },
+    recieved: {
+      nn: "Mottatt:",
+      nb: "Mottatt:",
+    },
+    dokumentasjonMangler: {
+      nn: "Dokumentasjon manglar",
+      nb: "Dokumentasjon mangler",
+    },
+    tarKontakt: {
+      nn: "Vi tek kontakt om vi treng meir informasjon",
+      nb: "Vi tar kontakt om vi trenger mer informasjon",
+    },
+  },
+};
