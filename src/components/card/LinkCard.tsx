ca962f90e22ba5627a9e95e7e955e7a14980e79f
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-05-23 12:40:11 +0200

    :arrow_up: Oppgraderer designsystem til v3

--- src/components/card/LinkCard.tsx
+++ src/components/card/LinkCard.tsx
@@ -1,4 +1,4 @@
-import { Next } from "@navikt/ds-icons";
+import { ChevronRightIcon } from "@navikt/aksel-icons";
 import styles from "./LinkCard.module.css";
 import { ReactElement } from "react";
 import { logNavigereEvent } from "../../utils/amplitude";
@@ -12,7 +12,7 @@ const LinkCard = ({ href, children }: Props) => {
   return (
     <a id={styles.linkcard} className={`navds-panel navds-link-panel`} href={href} onClick={() => logNavigereEvent()}>
       <div className="navds-link-panel__content">{children}</div>
-      <Next id={styles.linkcardChevron} className="navds-link-panel__chevron" />
+      <ChevronRightIcon id={styles.linkcardChevron} className="navds-link-panel__chevron" />
     </a>
   );
 };
