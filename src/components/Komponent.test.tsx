f4924f9cef4c6475bc997541bc2a4be945ade765
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-05-23 12:34:02 +0200

    :white_check_mark: Fikser test og legger til kjøring av tester i CI

--- src/components/Komponent.test.tsx
+++ src/components/Komponent.test.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { describe, expect, it } from "vitest";
-import { fireEvent, render, screen } from "../utils/test-utils";
+import { render, screen } from "../utils/test-utils";
 import Komponent from "./Komponent";
 
 describe("Simple working test for Komponent", () => {
   it("has a text", () => {
     render(<Komponent mottatt={new Date()} manglerVedlegg={true} />);
-    expect(screen.getByText("Søknad om AAP (arbeidsavklaringspenger)")).toBeDefined();
+    expect(screen.getByText("Søknad om AAP (arbeidsavklarings\u00ADpenger)")).toBeDefined();
   });
 });
