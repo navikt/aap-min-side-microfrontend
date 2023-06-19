af2b143f55ff88c1e001cbe0e2ab0eaeebc6a43e
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-06-19 14:07:47 +0200

    :speech_balloon: Fjerner overskrift siden denne blir lagt på av Min Side i ny versjon

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import useSWRImmutable from "swr/immutable";
 import { fetcher } from "./api/api";
 import { apiUrl } from "./api/urls";
 import Komponent from "./components/Komponent";
-import { Heading } from "@navikt/ds-react";
 import styles from "./App.module.css";
 import { Loading } from "./components/Loading";
-import { LanguageContext } from "./provider/LanguageProvider";
-import { useContext } from "react";
-import { text } from "./translations/text";
 
 function App() {
   const { data, isLoading } = useSWRImmutable(apiUrl, fetcher);
@@ -16,14 +12,9 @@ function App() {
     return null;
   }
 
-  const language = useContext(LanguageContext);
-
   return (
     <section>
       <div className={styles.mikrofrontend}>
-        <Heading className={styles.mikrofrontendHeader} level="2" size="medium">
-          {text.heading[language]}
-        </Heading>
         {isLoading ? (
           <Loading />
         ) : (
