12cb8abefdba187b6e857c9f7fa67e8b928587a2
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-06-28 13:17:13 +0200

    Fjernet max-widt propert og lar min-side-skallet ta seg av dette. Fjernet section for å bedre UU-opplevelse.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import useSWRImmutable from "swr/immutable";
 import { fetcher } from "./api/api";
 import { apiUrl } from "./api/urls";
 import Komponent from "./components/Komponent";
-import styles from "./App.module.css";
 import { Loading } from "./components/Loading";
 
 function App() {
@@ -13,15 +12,13 @@ function App() {
   }
 
   return (
-    <section>
-      <div className={styles.mikrofrontend}>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <Komponent mottatt={new Date(data[0]?.innsendtDato)} manglerVedlegg={data[0]?.manglendeVedlegg?.length > 0} />
-        )}
-      </div>
-    </section>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Komponent mottatt={new Date(data[0]?.innsendtDato)} manglerVedlegg={data[0]?.manglendeVedlegg?.length > 0} />
+      )}
+    </>
   );
 }
 
