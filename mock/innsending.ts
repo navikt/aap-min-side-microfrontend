d712625f394f62656c17fa50918d5630f82b7391
Øivind Stensrud <oivind.stensrud@nav.no>
2024-02-14 13:19:00 +0100

    :bug: Retter feil navn i testdata

--- mock/innsending.ts
+++ mock/innsending.ts
@@ -5,7 +5,7 @@ export default [
     response: () => {
       return [
         {
-          mottatDato: "2024-01-30T15:02:01.737467",
+          mottattDato: "2024-01-30T15:02:01.737467",
           journalpostId: "journalpostId",
           innsendingsId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
         },
